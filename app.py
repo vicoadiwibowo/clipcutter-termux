117066f4282fa25e82492526fb9ae3f5811d15d8
vicoadiwibowo <fchs85z4b5@privaterelay.appleid.com>
2026-07-26 07:25:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -168,10 +168,11 @@ CROP_1TO1 = (
     "x='(iw-min(iw\\,ih))/2':y='(ih-min(iw\\,ih))/2'"
 )
 
+# REVISI UKURAN UNTUK FORMAT 1:1
 SUBTITLE_STYLE = (
-    "FontName=Arial,FontSize=20,Bold=1,"
+    "FontName=Arial,FontSize=14,Bold=1,"
     "PrimaryColour=&H00FFFFFF,OutlineColour=&H00000000,"
-    "BorderStyle=1,Outline=2,Shadow=1,MarginV=30,Alignment=2"
+    "BorderStyle=1,Outline=1,Shadow=1,MarginV=25,Alignment=2"
 )
 
 
